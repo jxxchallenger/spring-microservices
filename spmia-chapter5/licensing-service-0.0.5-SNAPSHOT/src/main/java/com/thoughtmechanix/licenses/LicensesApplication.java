b0e823e7e522e0f8fc28b7323a37dc1e00988b2a
jxxchallenger <jxxchallenger@foxmail.com>
2019-08-15 21:01:26 +0800

    enable  Hystrix

--- spmia-chapter5/licensing-service-0.0.5-SNAPSHOT/src/main/java/com/thoughtmechanix/licenses/LicensesApplication.java
+++ spmia-chapter5/licensing-service-0.0.5-SNAPSHOT/src/main/java/com/thoughtmechanix/licenses/LicensesApplication.java
@@ -2,6 +2,7 @@ package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients //@EnableDiscoveryClient与@EnableFeignClients相互独一
+@EnableCircuitBreaker
 public class LicensesApplication {
 
     public static void main(String[] args) {
