62e2e5402961ffdc76f87568789dc2e9e1cfff71
jxxchallenger <jxxchallenger@foxmail.com>
2019-08-19 20:42:36 +0800

    Dynamically reload route configuration

--- spmia-chapter6/zuulsvr-0.0.6-SNAPSHOT/src/main/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
+++ spmia-chapter6/zuulsvr-0.0.6-SNAPSHOT/src/main/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
@@ -2,10 +2,12 @@ package com.thoughtmechanix.zuulsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RefreshScope
 public class ZuulServerApplication {
 
     public static void main(String[] args) {
